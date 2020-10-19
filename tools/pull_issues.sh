befce7e0891fd534667b4b9961e88f509f5b4cb1
Eneko <enekouru@gmail.com>
2020-10-19 20:01:15 +0200

    Script now checks if repo exists and removes it if it did not exist before

--- tools/pull_issues.sh
+++ tools/pull_issues.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-# Clone Brainhack Global 2020 repository to get project info.
-git clone https://github.com/brainhackorg/global2020
+# Filename of output file (it can include the path).
+OUTFILE=$1
+
+# Clone Brainhack Global 2020 repository to get project info and move into directory.
+if [[ -d "global2020" ]]
+then
+    echo "global2020 directory found."
+else
+    echo "The global2020 directory could not be found..."
+    git clone https://github.com/brainhackorg/global2020
+fi
+
+cd global2020
 
 # Get ID of projects on GitHub issues.
 ISSUE_ID_LIST=$(gh issue list -L 1000 -l "status:web_ready"| awk '{print $1}')
 
 # Create output file and add header.
-echo "ID \t TITLE \t LABELS \n" > projects.tsv
+echo "ID \t TITLE \t LABELS \n" > ${OUTFILE}
+
+echo "Pulling data from project issues... "
 
 # Loop throuh found issues.
 for ISSUE_ID in $ISSUE_ID_LIST
@@ -19,5 +32,23 @@ do
     LABELS_LIST=$(gh issue view "${ISSUE_ID}" | grep 'labels:' | cut -d':' -f2)
 
     # Save ID, title and labels to output file.
-    echo "${ISSUE_ID##*( )} \t ${TITLE##*( )} \t ${LABELS_LIST##*( )} \n" >> projects.tsv
+    echo "${ISSUE_ID##*( )} \t ${TITLE##*( )} \t ${LABELS_LIST##*( )} \n" >> ${OUTFILE}
 done
+
+# Leave directory.
+cd ..
+
+# Check if filename has a slash (path). Move the file one directory up if it's just a filename.
+if [[ "${OUTFILE}" != *\/*  ]] || [[ "${OUTFILE}" != *\\* ]]
+then
+    echo "An absolute path for the output file was not provided."
+    echo "Moving output file one directory up..."
+    mv global2020/${OUTFILE} .
+    CURRENT_DIR=$(pwd)
+    OUTFILE=$(echo "${CURRENT_DIR}/${OUTFILE}")
+fi
+
+echo "Removing cloned repository..."
+rm -rf global2020
+
+echo "Projects data saved in ${OUTFILE}"
