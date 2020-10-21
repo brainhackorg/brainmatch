acfd6af3df5c8debdf1a172b45e1b2c5a479995a
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2020-10-20 21:44:39 -0400

    ENH: Sort the top scores and serialize
    Sort the top scores and serialize.

--- brainmatch/brainmatch.py
+++ brainmatch/brainmatch.py
@@ -5,9 +5,16 @@ import argparse
 import functools
 import json
 import operator
+import os
 
 import pandas as pd
 
+extension_sep = "."
+top_match_label = "top"
+underscore = "_"
+
+project_top_id_label = "id"
+score_id_label = "score"
 
 project_id_field = "ID"
 project_title_field = "TITLE"
@@ -51,6 +58,45 @@ topic_label = "topic:"
 feature_keys = [git_skills_label, modality_label, programming_label,
                 project_type_label, project_tools_skills_label,
                 tools_label, topic_label]
+
+
+def _generate_top_match_column_names(n):
+
+    columns = []
+
+    for i in range(n):
+        project_top_id_col_name = \
+            project_top_id_label + underscore + top_match_label + str(i+1)
+        score_col_name = \
+            score_id_label + underscore + top_match_label + str(i+1)
+        columns.extend([project_top_id_col_name, score_col_name])
+
+    return columns
+
+
+def compute_top_n(match_df, n):
+
+    ids = match_df.columns.tolist()
+    project_count = len(ids[1:])
+    top_match_col_names = _generate_top_match_column_names(
+        n if n < project_count else project_count)
+    columns = list([[email_address_field], top_match_col_names])
+    columns = functools.reduce(operator.iconcat, columns, [])
+
+    top_match = []
+
+    # Loop over contributors and order project ids by their scores
+    for _, data in match_df.iterrows():
+        top_scores = data[1:].sort_values(ascending=False)
+        top_data = \
+            [x for y in zip(top_scores.index.tolist(), top_scores) for x in y]
+        top_data = list([[data[0]], top_data])
+        top_data = functools.reduce(operator.iconcat, top_data, [])
+        top_match.append(top_data)
+
+    top_match_df = pd.DataFrame(top_match, columns=columns)
+
+    return top_match_df
 
 
 def get_projects_features(project_data):
@@ -148,6 +194,8 @@ def _build_arg_parser():
                         help="Input contributors fields filename (.json).")
     parser.add_argument("out_match_fname", type=str,
                         help="Output match filename (.csv)")
+    parser.add_argument("--n", type=int, default=5,
+                        help="Top n.")
 
     return parser
 
@@ -179,6 +227,19 @@ def main():
 
     # Save data to a csv file
     match_df.to_csv(args.out_match_fname, index=False)
+
+    # Compute the top n
+    top_match_df = compute_top_n(match_df, args.n)
+
+    path = os.path.dirname(args.out_match_fname)
+    match_basename = os.path.basename(args.out_match_fname)
+    rootname, ext = match_basename.split(extension_sep)
+    top_basename = \
+        rootname + underscore + top_match_label + extension_sep + ext
+    top_fname = os.path.join(path, top_basename)
+
+    # Save data to a csv file
+    top_match_df.to_csv(top_fname, index=False)
 
 
 if __name__ == "__main__":
